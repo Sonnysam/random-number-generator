846e95a40eeae50cebd7fa6ed30389f8ac75536f
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2021-12-21 11:20:57 +0000

    Add files via upload

---
+++ #2 Random Number Generator/template.js
@@ -0,0 +1,13 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+const generateNumber = () => {
+    //Generate Number Between 1 and 10
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    number.innerHTML = randomNumber; 
+}
+
+btn.addEventListener ("click", generateNumber); 
+
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
